f45606f2f9986ae00b387c87a840bc1d308f4d68
Felipe P Silva <felipehendrix@programmer.net>
2018-10-24 20:49:27 -0300

    1-05 to 1-07

---
+++ 1-06.c
@@ -0,0 +1,14 @@
+//
+// Exercise 1-6
+//
+
+
+#include <stdio.h>
+
+
+main()
+{
+    int x;
+    x = getchar() != EOF;
+    printf("getchar() != EOF ?\n%d\n", x);
+}
---
+++ 1-07.c
@@ -0,0 +1,14 @@
+//
+// Exercise 1-7
+//
+
+
+#include <stdio.h>
+
+
+main()
+{
+	int c;
+	c = EOF;
+	printf("%d\n", c);
+}
